c581572fd986c275c20310e636fc5aeb3d273864
Robert Pirritano <rpirritano@gmail.com>
2017-06-23 16:09:46 -0700

    user-story-active-tasks: completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,21 @@
 var app = angular.module("blocitoff", ['firebase']);
 
 app.controller("SampleCtrl", function($scope, $firebaseObject, $firebaseArray) {
-  var ref = firebase.database().ref();
+  var ref = firebase.database().ref().child("tasks");
   // download the data into a local object
-  $scope.data = $firebaseArray(ref);
+  $scope.tasks = $firebaseArray(ref);
+  // $scope.newTask = null;
 
 
-  $scope.foo = $scope.data
+  $scope.FormSubmission = function(foo){
+    $scope.tasks.$add({task: foo})
+  }
+  //window.somedayAFormSubmission = function(){
+    //$scope.tasks.$add({task: "Create a form to do this!"})
+  //}
+  // $scope.foo = $scope.data
 
-  window.foo = $scope.data
+  // window.foo = $scope.data
   // putting a console.log here won't work, see below
 });
 
