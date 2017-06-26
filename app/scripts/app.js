8c0caa0a0cf457eb3f0dd9670536cbeab52289d2
Robert Pirritano <rpirritano@gmail.com>
2017-06-25 18:53:57 -0700

    hide old task: complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,31 @@
 var app = angular.module("blocitoff", ['firebase']);
 
-app.controller("SampleCtrl", function($scope, $firebaseObject, $firebaseArray) {
+app.controller("HomeCtrl", function($scope, $firebaseObject, $firebaseArray) {
   var ref = firebase.database().ref().child("tasks");
   // download the data into a local object
   $scope.tasks = $firebaseArray(ref);
   // $scope.newTask = null;
 
 
-  $scope.FormSubmission = function(foo){
-    $scope.tasks.$add({task: foo})
-  }
+  $scope.addTask = function(){
+    $scope.tasks.$add({
+      name: $scope.newTask,
+      completed: false,
+      expired: false,
+      createdAt: firebase.database.ServerValue.TIMESTAMP
+    });
+  };
+  
+  $scope.expiredTask = function(task) {
+    var currentTime = new Date();
+    if ((currentTime - task.createdAt) >= 604800000){
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
+
   //window.somedayAFormSubmission = function(){
     //$scope.tasks.$add({task: "Create a form to do this!"})
   //}
