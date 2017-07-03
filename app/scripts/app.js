da8fae87a39fab4fd3d4258514ffa45306e5db81
Robert Pirritano <rpirritano@gmail.com>
2017-07-03 09:25:07 -1000

    view completed tasks: completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,26 +6,67 @@ app.controller("HomeCtrl", function($scope, $firebaseObject, $firebaseArray) {
   $scope.tasks = $firebaseArray(ref);
   // $scope.newTask = null;
 
+  window.bar = $scope.tasks
+
+  $scope.completedTasks = $firebaseArray(ref.orderByChild("completed").equalTo(true))
+
+  
+
+ //to change button text
+ var textChange = $("a");
+$("a").on("click", function() {
+  var expiredLink = $(this);
+  if (expiredLink.text() == expiredLink.data("text-swap")) {
+    expiredLink.text(expiredLink.data("text-original"));
+  } else {
+    expiredLink.data("text-original", expiredLink.text());
+    expiredLink.text(expiredLink.data("text-swap"));
+  }
+});
+ 
+  //to hide/show completed tasks
+  $scope.completed = "taskObject.expired && taskObject.completed";
+  $scope.toggle = function() {
+    $scope.completed = !$scope.completed;
+  
+  };
+
+
+
 
   $scope.addTask = function(){
     $scope.tasks.$add({
       name: $scope.newTask,
+      priority: $scope.priority,
       completed: false,
       expired: false,
       createdAt: firebase.database.ServerValue.TIMESTAMP
     });
   };
   
-  $scope.expiredTask = function(task) {
-    var currentTime = new Date();
-    if ((currentTime - task.createdAt) >= 604800000){
+  $scope.expiredTask = function(taskObject) {
+    var currentTime = Date.now();
+
+    if ((currentTime - taskObject.createdAt) >= 604800000){
+      //console.log(1)
       return true;
     } else {
+     // console.log(2)
       return false;
     }
   };
   
+  $scope.priority = function(taskObject) {
 
+  }
+
+  $scope.complete = function(taskObject) {
+    taskObject.completed = true
+    $scope.tasks.$save(taskObject)
+
+  }
+  
+  
   //window.somedayAFormSubmission = function(){
     //$scope.tasks.$add({task: "Create a form to do this!"})
   //}
